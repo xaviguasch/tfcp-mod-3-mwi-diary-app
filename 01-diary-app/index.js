9cab38d4806cafadbcac24de7e58ff8eadb5f923
xaviguasch <xaviguasch@gmail.com>
2020-07-14 12:38:46 +0200

    added the user's input to the dom

--- 01-diary-app/index.js
+++ 01-diary-app/index.js
@@ -1,5 +1,13 @@
 const entryForm = document.getElementById('entryForm')
+const entriesSection = document.getElementById('entries')
+const entryTextbox = document.querySelector('.entry-textbox')
 
 entryForm.addEventListener('submit', (e) => {
   e.preventDefault()
+
+  const entryDiv = document.createElement('div')
+  entryDiv.className = 'singe-entry'
+  entryDiv.innerText = entryTextbox.value
+
+  entriesSection.append(entryDiv)
 })
